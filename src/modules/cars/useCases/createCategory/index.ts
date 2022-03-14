4356c00bf874b35952db7ccb3c5cdaf4ba550384
Rodrigo Monteiro Silva <rodrigo.monteiro.silva@outlook.com>
2022-03-14 16:02:06 -0300

    second commit

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,17 +1,17 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { CreateCategoryController } from "./CreateCategoryController";
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+// import { CreateCategoryController } from "./CreateCategoryController";
+// import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-export default (): CreateCategoryController => {
-  const categoriesRepositories = new CategoriesRepository();
+// export default (): CreateCategoryController => {
+//   const categoriesRepositories = new CategoriesRepository();
 
-  const createCategoryUseCase = new CreateCategoryUseCase(
-    categoriesRepositories
-  );
+//   const createCategoryUseCase = new CreateCategoryUseCase(
+//     categoriesRepositories
+//   );
 
-  const createCategoryController = new CreateCategoryController(
-    createCategoryUseCase
-  );
+//   const createCategoryController = new CreateCategoryController(
+//     createCategoryUseCase
+//   );
 
-  return createCategoryController;
-};
+//   return createCategoryController;
+// };
