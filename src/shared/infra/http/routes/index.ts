3a1b39175d0c166ab940582a88a580b4ce11716a
Rodrigo Monteiro Silva <rodrigo.monteiro.silva@outlook.com>
2022-04-11 19:03:13 -0300

    tests

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -13,7 +13,7 @@ router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
-router.use("/rental", rentalRoutes);
+router.use("/rentals", rentalRoutes);
 router.use(authenticateRoutes);
 
 export { router };
